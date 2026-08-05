29bf2b5f89d6776ecbd2243c8d65b55abc69fd10
JingChen-Thu <cjcjyu597460474@163.com>
2026-08-05 11:33:35 +0800

    update bash

--- CHS/Step3_bash_1d_model_inversion.sh
+++ CHS/Step3_bash_1d_model_inversion.sh
@@ -8,11 +8,11 @@ TomoATT_path=TOMOATT
 # 并行的processor数量
 Nproc=8
 
-# mpirun -n ${Nproc} ${TomoATT_path} -i 3_input_params/input_params_step1_1D_inv.yaml
 
+mpirun -n ${Nproc} ${TomoATT_path} -i 3_input_params/input_params_step1_1D_inv.yaml
 # 对于WSL系统，当用root用户运行时，需要添加 --allow-run-as-root
 # 当Nproc大于物理核心数时，需要 --oversubscribe 
-mpirun -n ${Nproc} --allow-run-as-root --oversubscribe ${TomoATT_path} -i 3_input_params/input_params_step1_1D_inv.yaml
+# mpirun -n ${Nproc} --allow-run-as-root --oversubscribe ${TomoATT_path} -i 3_input_params/input_params_step1_1D_inv.yaml
 
 # 将生成的模型文件备份一份到主目录
 cp ./OUTPUT_FILES/OUTPUT_FILES_step1_1D_inv/final_model.h5 ./2_models/model_1d_after_1d_inv.h5
--- CHS/Step5_bash_real_data_inversion.sh
+++ CHS/Step5_bash_real_data_inversion.sh
@@ -8,13 +8,22 @@ TomoATT_path=TOMOATT
 # 并行的processor数量
 Nproc=8
 
-# 对于WSL系统，当用root用户运行时，需要添加 --allow-run-as-root
-# 当Nproc大于物理核心数时，需要 --oversubscribe 
 # ------- 第一步，地震重定位 --------
-# mpirun -n ${Nproc} --allow-run-as-root --oversubscribe ${TomoATT_path} -i 3_input_params/input_params_step2_reloc.yaml
+mpirun -n ${Nproc} ${TomoATT_path} -i 3_input_params/input_params_step2_reloc.yaml
 
 # ------- 第二步，各向异性速度反演+地震定位 --------
 # 进行10次迭代进行示例
-# mpirun -n ${Nproc} --allow-run-as-root --oversubscribe ${TomoATT_path} -i 3_input_params/input_params_step3_inv_reloc.yaml
+mpirun -n ${Nproc} ${TomoATT_path} -i 3_input_params/input_params_step3_inv_reloc.yaml
 # 进行100次迭代，得到完整结果
-mpirun -n ${Nproc} --allow-run-as-root --oversubscribe ${TomoATT_path} -i 3_input_params/input_params_step3_inv_reloc_100iter.yaml
+mpirun -n ${Nproc} ${TomoATT_path} -i 3_input_params/input_params_step3_inv_reloc_100iter.yaml
+
+# # 对于WSL系统，当用root用户运行时，需要添加 --allow-run-as-root
+# # 当Nproc大于物理核心数时，需要 --oversubscribe 
+# # ------- 第一步，地震重定位 --------
+# mpirun -n ${Nproc} --allow-run-as-root --oversubscribe  ${TomoATT_path} -i 3_input_params/input_params_step2_reloc.yaml
+
+# # ------- 第二步，各向异性速度反演+地震定位 --------
+# # 进行10次迭代进行示例
+# mpirun -n ${Nproc} --allow-run-as-root --oversubscribe  ${TomoATT_path} -i 3_input_params/input_params_step3_inv_reloc.yaml
+# # 进行100次迭代，得到完整结果
+# mpirun -n ${Nproc} --allow-run-as-root --oversubscribe  ${TomoATT_path} -i 3_input_params/input_params_step3_inv_reloc_100iter.yaml
